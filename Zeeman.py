1b5804a20dbf9495d7d8316398279fb73b20c01d
Martin A Valenzuela <Mars110110@aol.com>
2014-02-20 16:06:46 -0600

    Include Ternary search

--- Zeeman.py
+++ Zeeman.py
@@ -97,6 +97,21 @@ def dataArray(peakL1, peakR1, peakL2, peakI, peakO):
     radArray = [rad1, rad2, radI, radO]
     return radArray
 
+#Ternary Search
+def ternarySearch(f, left, right, absolutePrecision):
+    #left and right are the current bounds; the maximum is between them
+    if (right - left) < absolutePrecision:
+        return (left + right)/2
+
+    leftThird = (2*left + right)/3
+    rightThird = (left + 2*right)/3
+
+    if f(leftThird) < f(rightThird):
+        return ternarySearch(f, leftThird, right, absolutePrecision)
+    else:
+        return ternarySearch(f, left, rightThird, absolutePrecision)
+
+
 
 def main():
     fileNum = input("Type in file number: ")
